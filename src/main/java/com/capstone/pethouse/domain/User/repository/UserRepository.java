e2b77d3fe157ad87f50b4e3f437ec0e94c030e0c
yoojanghun <yoojanghun@naver.com>
2026-04-29 15:53:07 +0900

    #49 - 사용자 검색 시 searchQuery, searchType 사용할 수 있도록 변경
    기존의 코드에선 searchQuery, searchType등을 인자로만 받고, 전혀 사용하지 않았음.
    사용자 User를 검색할 때, searchType, searchQuery등을 사용하여 검색을 할 수 있도록 메서드 추가.

--- src/main/java/com/capstone/pethouse/domain/User/repository/UserRepository.java
+++ src/main/java/com/capstone/pethouse/domain/User/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.capstone.pethouse.domain.User.repository;
 
 import com.capstone.pethouse.domain.User.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -16,4 +20,19 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByMemberNameAndMemberPhone(String memberName, String memberPhone);
 
     Optional<User> findByMemberIdAndMemberNameAndMemberPhone(String memberId, String memberName, String memberPhone);
+
+    @Query("SELECT u FROM User u WHERE " +
+            "(:searchQuery IS NULL OR :searchQuery = '' OR " +
+            " (:searchType = 'memberId' AND u.memberId LIKE %:searchQuery%) OR " +
+            " (:searchType = 'memberName' AND u.memberName LIKE %:searchQuery%) OR " +
+            " (:searchType = 'memberPhone' AND u.memberPhone LIKE %:searchQuery%) OR " +
+            " (:searchType = 'roleCode' AND CAST(u.roleCode AS string) LIKE %:searchQuery%) OR " +
+            " (:searchType IS NULL AND (" +
+            "    u.memberId LIKE %:searchQuery% OR " +
+            "    u.memberName LIKE %:searchQuery% OR " +
+            "    u.memberPhone LIKE %:searchQuery% OR " +
+            "    CAST(u.roleCode AS string) LIKE %:searchQuery%)))")
+    Page<User> findAllWithSearch(@Param("searchType") String searchType,
+                                 @Param("searchQuery") String searchQuery,
+                                 Pageable pageable);
 }
