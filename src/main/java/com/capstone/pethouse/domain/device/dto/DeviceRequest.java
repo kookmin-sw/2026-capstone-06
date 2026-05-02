4625fb25a167f79cfa3def8aa1c2d9200840b0cb
yoojanghun <yoojanghun@naver.com>
2026-05-02 15:12:44 +0900

    #49 - DeviceRequest 수정
    petHouseId를 입력받을 수 있도록, 수정
    Device관련 요청을 할 때, 반려동물의 이름, 종류, 생년월일 등의 정보는 필요 없을 것으로 보임

--- src/main/java/com/capstone/pethouse/domain/device/dto/DeviceRequest.java
+++ src/main/java/com/capstone/pethouse/domain/device/dto/DeviceRequest.java
@@ -4,12 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record DeviceRequest(
         Long seq,
-        @JsonProperty("device_id") String deviceId,
+        @JsonProperty("house_id") Long petHouseId,
         @JsonProperty("member_id") String memberId,
         @JsonProperty("serial_num") String serialNum,
-        @JsonProperty("old_serial_num") String oldSerialNum,
-        @JsonProperty("object_code") String objectCode,
-        @JsonProperty("object_birth") String objectBirth,
-        @JsonProperty("device_type") String deviceType
+        @JsonProperty("device_type") String deviceType,
+        @JsonProperty("device_id") String deviceId,
+        @JsonProperty("old_serial_num") String oldSerialNum
 ) {
 }
