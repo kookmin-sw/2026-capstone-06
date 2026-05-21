9386f1c6444d6f45e6625a12a8b7a70fe5d6e67c
yoojanghun <yoojanghun@naver.com>
2026-05-22 01:08:45 +0900

    CI CD  위치 변경 및 CORS 설정 추가

--- backend/src/main/java/com/capstone/pethouse/global/config/SecurityConfig.java
+++ backend/src/main/java/com/capstone/pethouse/global/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "http://localhost:8080", "https://2026-capstone-06.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
