c000d7cbbef586454ca7594e4125b2fb6d86875f
yoojanghun <yoojanghun@naver.com>
2026-05-28 22:19:50 +0900

    frontend에서 새로운 domain 주소로 변경

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://ec2-54-116-38-250.ap-northeast-2.compute.amazonaws.com:8081',
+        target: 'https://api.pethousefinder.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         ws: true,
